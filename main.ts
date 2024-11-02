ed959591c1fd292fd650ec99a48a8456086451fd
yul <aymeric.guth@protonmail.com>
2024-11-02 12:19:29 +0100

    Add patterns component

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ import {
 	// @ts-ignore
 	HTMLElement,
 	addIcon,
+	CachedMetadata,
 } from "obsidian";
 // @ts-ignore
 import {
@@ -26,6 +27,8 @@ import {
 	Generator,
 	// @ts-ignore
 	notify,
+	// @ts-ignore
+	Assert,
 } from "./api";
 // @ts-ignore
 import { Paths, Status, Types, Namespace, Default } from "./constants";
@@ -41,13 +44,6 @@ interface MyPluginSettings {
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: "default",
 };
-
-// représente la vue actuelle, l'onglet actif, le focus
-// contient une ref au frontmatter qui est preprocess,
-// ie champs fm.project, fm.area, ... sont populés
-class Current {
-	constructor() { }
-}
 
 const dayShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const monthShort = [
@@ -140,6 +136,40 @@ export default class MyPlugin extends Plugin {
 		const abstractPath = app.vault.getAbstractFileByPath(file);
 		// @ts-ignore
 		return app.metadataCache.getFileCache(abstractPath);
+	}
+
+	extractUUIDFromLink(link: string) {
+		Assert.True(
+			link.length >= 36,
+			`extractUUIDFromLink: Invalid parameter type: link: "${link}"`,
+		);
+
+		// at this point link is either form uuid#heading || invalid
+		// we can safely assume 0:36 is the uuid
+		Assert.True(
+			Helper.isUUID(link.slice(0, 36)),
+			`extractUUIDFromLink: Invalid parameter type: link: "${link}"`,
+		);
+		if (link.length === 36) {
+			return link;
+		} else {
+			return link.slice(0, 36);
+		}
+	}
+
+	getFileCacheFromUUID(_id: string): CachedMetadata {
+		for (const f of this.vaultContent) {
+			if (f.path.length < 36) {
+				continue;
+			}
+
+			const path = f.path.split("/");
+			if (path[path.length - 1].slice(0, -3) === _id) {
+				return app.metadataCache.getFileCache(
+					this.app.vault.getAbstractFileByPath(f.path),
+				);
+			}
+		}
 	}
 
 	sneakyTabRenamer(app) {
@@ -256,6 +286,7 @@ export default class MyPlugin extends Plugin {
 		this.frontmatter = new Frontmatter(this);
 		this.listMaker = new ListMaker(this, this.dv, this.frontmatter);
 		this.generate = new Generator(this.app);
+		this.vaultContent = this.app.vault.getFiles();
 
 		this.api = {
 			getArea: Helper.getArea,
@@ -286,12 +317,9 @@ export default class MyPlugin extends Plugin {
 			app: this.app,
 		};
 		addIcon(
-
 			"GoNextIcon",
-		`<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="currentColor" d="M20 12V2h2v10zm4 0V2h2v10zm4 0V2h2v10zm-12 8a3.912 3.912 0 0 1-4-4a3.912 3.912 0 0 1 4-4v-2a6 6 0 1 0 6 6h-2a3.912 3.912 0 0 1-4 4"></path><path fill="currentColor" d="M28.893 18.454L26.098 16l-1.318 1.504l2.792 2.452l-2.36 4.088l-3.427-1.16a9.032 9.032 0 0 1-2.714 1.565L18.36 28h-4.72l-.71-3.55a9.095 9.095 0 0 1-2.695-1.572l-3.447 1.166l-2.36-4.088l2.725-2.395a8.926 8.926 0 0 1-.007-3.128l-2.718-2.39l2.36-4.087l3.427 1.16A9.03 9.03 0 0 1 12.93 7.55L13.64 4H16V2h-2.36a2 2 0 0 0-1.961 1.608l-.504 2.519a10.967 10.967 0 0 0-1.327.753l-2.42-.819a1.998 1.998 0 0 0-2.372.895l-2.36 4.088a2 2 0 0 0 .411 2.502l1.931 1.697C5.021 15.495 5 15.745 5 16c0 .258.01.513.028.766l-1.92 1.688a2 2 0 0 0-.413 2.502l2.36 4.088a1.998 1.998 0 0 0 2.374.895l2.434-.824a10.974 10.974 0 0 0 1.312.759l.503 2.518A2 2 0 0 0 13.64 30h4.72a2 2 0 0 0 1.961-1.608l.504-2.519a10.967 10.967 0 0 0 1.327-.753l2.419.818a1.998 1.998 0 0 0 2.373-.894l2.36-4.088a2 2 0 0 0-.411-2.502"></path></svg>`,
+			`<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="currentColor" d="M20 12V2h2v10zm4 0V2h2v10zm4 0V2h2v10zm-12 8a3.912 3.912 0 0 1-4-4a3.912 3.912 0 0 1 4-4v-2a6 6 0 1 0 6 6h-2a3.912 3.912 0 0 1-4 4"></path><path fill="currentColor" d="M28.893 18.454L26.098 16l-1.318 1.504l2.792 2.452l-2.36 4.088l-3.427-1.16a9.032 9.032 0 0 1-2.714 1.565L18.36 28h-4.72l-.71-3.55a9.095 9.095 0 0 1-2.695-1.572l-3.447 1.166l-2.36-4.088l2.725-2.395a8.926 8.926 0 0 1-.007-3.128l-2.718-2.39l2.36-4.087l3.427 1.16A9.03 9.03 0 0 1 12.93 7.55L13.64 4H16V2h-2.36a2 2 0 0 0-1.961 1.608l-.504 2.519a10.967 10.967 0 0 0-1.327.753l-2.42-.819a1.998 1.998 0 0 0-2.372.895l-2.36 4.088a2 2 0 0 0 .411 2.502l1.931 1.697C5.021 15.495 5 15.745 5 16c0 .258.01.513.028.766l-1.92 1.688a2 2 0 0 0-.413 2.502l2.36 4.088a1.998 1.998 0 0 0 2.374.895l2.434-.824a10.974 10.974 0 0 0 1.312.759l.503 2.518A2 2 0 0 0 13.64 30h4.72a2 2 0 0 0 1.961-1.608l.504-2.519a10.967 10.967 0 0 0 1.327-.753l2.419.818a1.998 1.998 0 0 0 2.373-.894l2.36-4.088a2 2 0 0 0-.411-2.502"></path></svg>`,
 		);
-			
-		
 
 		this.addRibbonIcon("GoNextIcon", "[g]o[n]ext", async () => {
 			this.generate.fleeting();
@@ -376,33 +404,6 @@ export default class MyPlugin extends Plugin {
 			},
 		});
 
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "gonext-utils-copy-current-file-uuid",
-			name: "Copy current file UUID",
-			// @ts-ignore
-			callback: () => {
-				// @ts-ignore
-				const fm = this.gonext.getCurrentFrontmatter();
-				// @ts-ignore
-				if (fm === undefined) {
-					// console.warn(
-					// 	"Current file does not have a valid `frontmatter`",
-					// );
-					return;
-				}
-
-				if (fm.uuid == undefined) {
-					// console.warn("Current file does not have a valid `UUID`");
-					return;
-				}
-
-				// console.log("editor callback function");
-				// console.log(editor.getSelection());
-				// editor.replaceSelection('Sample Editor Command');
-			},
-		});
-
 		this.addCommand({
 			id: "gonext-generate-fleeting",
 			name: "Generate fleeting note",
@@ -411,43 +412,6 @@ export default class MyPlugin extends Plugin {
 				this.generate.fleeting();
 			},
 		});
-
-		// this.addCommand({
-		// 	id: "gonext-generate-fleeting",
-		// 	name: "Generate fleeting note",
-		// 	// @ts-ignore
-		// 	callback: () => {
-		// 		const dt = new Date();
-		// 		const note = {
-		// 			uuid: uuidv4(),
-		// 			type: 13,
-		// 			version: "0.0.4",
-		// 			created_at: dt.toISOString(),
-		// 			path: "",
-		// 			data: "",
-		// 		};
-		//
-		// 		note.path = `800 Inbox/${note.uuid}.md`;
-		// 		note.data = `---\ntype: 13\nuuid: "${note.uuid}"\ncreated_at: "${note.created_at}"\nversion: "0.0.4"\n---\n## Content\n`;
-		//
-		// 		const f = this.app.vault
-		// 			.create(note.path, note.data)
-		// 			.then((f) => {
-		// 				return f;
-		// 			});
-		// 		const active = this.app.workspace.activeLeaf;
-		// 		// @ts-ignore
-		// 		const root = active.parent;
-		// 		this.app.workspace.createLeafInParent(
-		// 			root,
-		// 			root.children.length + 1,
-		// 		);
-		// 		const leaf = root.children[root.children.length - 1];
-		// 		f.then((file) => {
-		// 			leaf.openFile(file, { active: true });
-		// 		});
-		// 	},
-		// });
 
 		this.addCommand({
 			id: "safe-delete",
@@ -542,6 +506,168 @@ export default class MyPlugin extends Plugin {
 				app.workspace.openLinkText(parent, "/", false);
 			},
 		});
+
+		this.loadIndex();
+	}
+
+	loadIndex() {
+		console.log("gonext - loadIndex()");
+		const indexPath = this.app.vault.getAbstractFileByPath("Index.md");
+		const index = this.app.metadataCache.getFileCache(indexPath);
+		const domains = [];
+
+		{
+			let start = undefined;
+			let end = undefined;
+			let found = false;
+
+			// recherche du top level heading `## domains`
+			for (const heading of index.headings) {
+				if (heading.level === 2 && found) {
+					end = heading.position.start.offset;
+					break;
+				} else if (
+					heading.level === 2 &&
+					heading.heading === "domains"
+				) {
+					start = heading.position.end.offset;
+					found = true;
+				}
+			}
+
+			// recherche des liens qui sont contenus dans les bornes du top level heading `## domains`
+			for (const link of index.links) {
+				const pos = link.position;
+				if (pos.start.offset < start || pos.end.offset > end) {
+					continue;
+				}
+
+				const indexContent = this.getFileCacheFromUUID(link.link);
+				if (indexContent.headings.length < 2) {
+					console.error(`Invalid Index: ${link.link}`);
+					continue;
+				}
+
+				domains.push(indexContent);
+			}
+		}
+
+		// recherche dans les index des domains respectifs s'il y a présence d'une entry `### patterns`
+		// -> components
+		const components = {};
+		for (const domain of domains) {
+			const name = domain.headings[1].heading;
+			for (const component of domain.links) {
+				if (component.displayText === "patterns") {
+					if (components[name] === undefined) {
+						components[name] = {};
+					}
+
+					const patternsContent = this.getFileCacheFromUUID(
+						this.extractUUIDFromLink(component.link),
+					);
+					if (patternsContent.links.length === 0) {
+						continue;
+					}
+
+					if (components[name]["patterns"] === undefined) {
+						components[name]["patterns"] = [];
+					}
+
+					for (const link of patternsContent.links) {
+						components[name]["patterns"].push(
+							this.getFileCacheFromUUID(
+								this.extractUUIDFromLink(link.link),
+							),
+						);
+					}
+				}
+			}
+		}
+
+		// a ce point on a le root de l'arbre de chaque domain
+		// reste maintenant à
+		// parser chaque fiche pattern
+		// traverser l'arbre formé par chaque domain
+		// le mettre sous une forme qui soit plus facilement exploitable
+		// comment faire la distiction entre une note contenant un index de pattern et une sequence?
+		// une sequence suit le format
+		// ### name
+		// #### steps
+		// #### (total)
+		// #### (cost)
+		// #### (cues)
+		// on peut suivre la logique suivante: si la note contient un et un seul heading 3 et un et un seul heading 4 appelé steps c'est une sequence
+		// si elle contient un et un seul heading 3 et pas de heading 4, c'est une pattern, chaque lien devra etre exploré
+
+		// on fait la loop pour un domain
+		// zzz
+
+		// a ce point on est sur le heading ### patterns du domain cible
+		const q = [];
+		const results = [];
+
+		for (const pattern of components["zzz"]["patterns"]) {
+			q.push([["zzz", "patterns"], pattern]);
+		}
+
+		// les links doivent etre in bound dans `### name`
+
+		while (q.length > 0) {
+			const [path, note] = q.shift();
+			// console.log(note)
+			// console.log(path)
+			// console.log(note.headings[1].heading)
+			if (this.isSequence(note)) {
+				// easy
+				// on résout la séquence
+				// on append aux results
+				// domain name / patterns / pattern name a / pattern name b / sequence name -> uuid | file cache | file path
+				results.push([[...path, note.headings[1].heading], note]);
+
+			} else {
+				const nameHeading = note.headings[1];
+				const start = nameHeading.position.end.offset;
+				let stop = 0;
+				if (note.headings[2] === undefined) {
+					stop =
+						note.sections[note.sections.length - 1].position.end.offset;
+				} else {
+					stop = note.headings[2].position.start.offset;
+				}
+				///
+				for (const link of note.links) {
+					// link in bound nameHeading
+					if (link.position.start.offset < start || link.position.end.offset > stop) {
+						continue;
+					}
+
+					const f = this.getFileCacheFromUUID(
+						this.extractUUIDFromLink(link.link),
+					);
+					q.push([[...path, note.headings[1].heading], f]);
+				}
+			}
+		}
+		// console.log(results);
+		for (const [path, note] of results) {
+			console.log(path);
+			// console.log(note)
+		}
+	}
+
+	isSequence(note: CachedMetadata): boolean {
+		Assert.True(
+			note.headings.length > 0,
+			`isSequence: Invalid parameter: note: ${note.frontmatter.uuid}`,
+		);
+		for (const heading of note.headings) {
+			if (heading.level === 4 && heading.heading === "steps") {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	onunload() {
